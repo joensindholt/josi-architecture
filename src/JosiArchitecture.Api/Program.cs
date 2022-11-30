ea9afb6cef3002f320313317ae7cfe7d19d4f76c
Joen Sindholt <joensindholt@gmail.com>
2022-11-30 19:31:07 +0100

    Add AzureAppServices logging

--- src/JosiArchitecture.Api/Program.cs
+++ src/JosiArchitecture.Api/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore;
+﻿using Microsoft.Extensions.Logging.AzureAppServices;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JosiArchitecture.Api
 {
@@ -12,6 +15,22 @@ namespace JosiArchitecture.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddAzureWebAppDiagnostics();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.Configure<AzureFileLoggerOptions>(options =>
+                    {
+                        options.FileName = "api-log";
+                        options.FileSizeLimit = 50 * 1024;
+                        options.RetainedFileCountLimit = 10;
+                    });
+                })
                 .UseStartup<Startup>();
     }
 }

918009a87d7f192c76b6b0cb506508330a04c796
Joen Sindholt <joensindholt@gmail.com>
2022-12-20 15:32:29 +0100

    Add NgRx dev tools

--- src/JosiArchitecture.AngularApp/src/app/app.module.ts
+++ src/JosiArchitecture.AngularApp/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
@@ -20,8 +22,17 @@ import { TodosComponent } from './todos/todos.component';
     HttpClientModule,
     StoreModule.forRoot({ count: counterReducer, todos: todosReducer }, {}),
     EffectsModule.forRoot([TodoEffects]),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75 // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

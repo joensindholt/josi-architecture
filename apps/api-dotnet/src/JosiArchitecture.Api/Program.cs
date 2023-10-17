15f395494a6612c546033bb5d180177ab758fc89
Joen Sindholt <joensindholt@gmail.com>
2023-10-17 15:51:30 +0200

    Follow nx guide on how to do github actions #6

--- apps/api-dotnet/src/JosiArchitecture.Api/Program.cs
+++ apps/api-dotnet/src/JosiArchitecture.Api/Program.cs
@@ -27,7 +27,7 @@ namespace JosiArchitecture.Api
 {
     public class Program
     {
-        const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        const string MyAllowSpecificOrigins = "myAllowSpecificOrigins";
 
         public static void Main(string[] args)
         {
